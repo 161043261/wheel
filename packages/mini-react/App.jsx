c40ded5ad9320f14fabdf53dcfccd93fc980665d
hangtiancheng <hangtiancheng@bytedance.com>
2025-07-25 16:03:47 +0800

    chore: Regular code maintenance

--- packages/mini-react/App.jsx
+++ packages/mini-react/App.jsx
@@ -1,15 +1,21 @@
 /** @jsx React.createElement */
 import React from "./core/React.js";
 
-function Foo() {
-  const [cnt, setCnt] = React.useState(0);
-  const [cnt2, setCnt2] = React.useState(1);
+function Demo() {
+  const [cnt, setCnt] = React.useState(1);
+  const [cnt2, setCnt2] = React.useState(2);
+  const [cnt3, setCnt3] = React.useState(3);
 
   const handleClick = () => {
     setCnt(cnt + 1);
   };
+
   const handleClick2 = () => {
-    setCnt2((cnt2) => cnt2);
+    setCnt2((cnt2) => cnt2 + 1);
+  };
+
+  const handleClick3 = () => {
+    setCnt3((cnt3) => cnt3);
   };
 
   React.useEffect(() => {
@@ -33,12 +39,23 @@ function Foo() {
     };
   }, [cnt2]);
 
+  React.useEffect(() => {
+    console.log(`cnt3: ${cnt3}`);
+    return () => {
+      console.log("cnt3 Cleanup");
+    };
+  }, [cnt3]);
+
   return (
     <div>
-      <div>Foo cnt {cnt}</div>
+      <div>cnt: {cnt}</div>
       <button onClick={handleClick}>addCnt</button>
-      <div>Foo cnt {cnt2}</div>
+
+      <div>cnt2: {cnt2}</div>
       <button onClick={handleClick2}>addCnt2</button>
+
+      <div>cnt3: {cnt3}</div>
+      <button onClick={handleClick3}>addCnt3</button>
     </div>
   );
 }
@@ -46,7 +63,7 @@ function Foo() {
 export default function App() {
   return (
     <div id="app">
-      <Foo />
+      <Demo />
     </div>
   );
 }
